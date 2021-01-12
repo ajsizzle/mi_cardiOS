47d3e3dfb7bf32cf0857914d75788ee1ce4e4090
Alex Johnson <ajohnson2389@gmail.com>
2021-01-12 12:40:08 -0500

    Initial Commit

---
+++ MiCard/MiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiCardApp.swift
+//  MiCard
+//
+//  Created by Alex Johnson on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
