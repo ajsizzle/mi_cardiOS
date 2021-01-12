578b3bacc53e8e8276d770b7cda0ff77fb8cd222
Alex Johnson <ajohnson2389@gmail.com>
2021-01-12 15:28:45 -0500

    initial commit

--- MiCard/ContentView.swift
+++ MiCard/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.20, green: 0.60, blue: 0.86)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("alexprofile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 250)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5))
+                Text("Alexander Johnson")
+                    .font(Font.custom("MarckScript-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(.top, -15.0)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(Font.custom("Poppins-Regular", size: 20))
+                Divider()
+                InfoView(text: "321-890-7230", imageName: "phone.fill")
+                InfoView(text: "ajohnson2389@gmail.com", imageName: "envelope.fill")
+            }
+        }
+
     }
 }
 
@@ -19,3 +42,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
---
+++ MiCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  MiCard
+//
+//  Created by Alex Johnson on 1/12/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                Text(text)
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
